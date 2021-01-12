d0313482dac95c0b0f6dc6011bd393f99b5b7446
Alex <alexandra_dinisor@yahoo.com>
2021-01-12 23:01:26 +0200

    Add document classifier using Naive Bayes

--- load_dataset.py
+++ load_dataset.py
@@ -1,5 +1,7 @@
+import math
+import operator
 import os
-from collections import defaultdict
+from collections import Counter, defaultdict
 
 import nltk
 from nltk.corpus import wordnet
@@ -7,9 +9,16 @@ from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import sent_tokenize, word_tokenize
 
 
-def get_articles_dict():
-    articles_dir = 'BBC News Summary/News Articles/'
-    articles_dict_data = defaultdict(dict)
+class DocumentProcessor(object):
+    def __init__(self, orig_doc=None, word_tokenizer=None,  summarised_doc=None):
+        self.orig_doc = orig_doc
+        self.word_tokenizer = word_tokenizer
+        self.summarised_doc = summarised_doc
+
+
+def get_documents_dict(articles_dir):
+
+    documents_dict_data = defaultdict(dict)
 
     for d in os.scandir(articles_dir):
         if d.is_dir():
@@ -20,22 +29,14 @@ def get_articles_dict():
                 with open(absolute_path, encoding="utf8", errors='ignore') as article_file:
                     # print(absolute_path)
                     data = article_file.read()
-                    articles_dict_data[d.name][f] = data
+                    documents_dict_data[d.name][f] = data
                     # final_dict[d.name][f] = data
                     # exit(0)
-
-    # print(articles_dict_data['business']['006.txt'])
-    # print('_________________')
-    return articles_dict_data
+    return documents_dict_data
 
 
-# get_articles_dict()
-print('_____________')
-print()
+def get_summaries_dict(summaries_dir):
 
-
-def get_summaries_dict():
-    summaries_dir = 'BBC News Summary/Summaries/'
     summaries_dict_data = defaultdict(dict)
 
     for d in os.scandir(summaries_dir):
@@ -45,89 +46,67 @@ def get_summaries_dict():
                 absolute_path = d.path + '/' + f
 
                 with open(absolute_path, encoding="utf8", errors='ignore') as article_file:
-                    # print(absolute_path)
+
                     data = article_file.read()
                     summaries_dict_data[d.name][f] = data
-                    # final_dict[d.name][f] = data
-                    # exit(0)
-    # print(summaries_dict_data['business']['006.txt'])
-    # print('_________________')
+
     return summaries_dict_data
 
 
-# get_summaries_dict()
+def get_final_dict(docs_dir, summaries_dir):
+    set_dict = defaultdict(dict)
+
+    docs_dict = get_documents_dict(docs_dir)
+
+    summaries_dict = get_summaries_dict(summaries_dir)
+
+    for key_class in docs_dict:
+        for key_doc in docs_dict[key_class]:
+            doc = docs_dict[key_class][key_doc]
+            word_tokens = word_tokenize(doc)
+
+            set_dict[key_class][key_doc] = DocumentProcessor(
+                orig_doc=doc,
+                word_tokenizer=word_tokens,
+                summarised_doc=summaries_dict[key_class][key_doc])
+
+    return set_dict
 
 
-def get_final_articles_dict():
-    final_dict = defaultdict(dict)
-    articles_dict_data = get_articles_dict()
-    summaries_dict_data = get_summaries_dict()
+docs_training_dir = 'Training Set/News Articles/'
+summaries_training_dir = 'Training Set/Summaries/'
+training_set_dict = get_final_dict(docs_training_dir, summaries_training_dir)
 
-    for key_category in articles_dict_data:
-
-        for key_doc in articles_dict_data[key_category]:
-            final_dict[key_category][key_doc] = [articles_dict_data[key_category]
-                                                 [key_doc], summaries_dict_data[key_category][key_doc]]
-    return final_dict
-
-
-# mystr = get_final_articles_dict()['business']['006.txt'][0]
-
+docs_test_dir = 'Test Set/News Articles/'
+summaries_test_dir = 'Test Set/Summaries/'
+test_set_dict = get_final_dict(docs_test_dir, summaries_test_dir)
 # tokenizer in words and sentecizer
-
-
-def get_word_tokenizer():
-    word_tokenizer = defaultdict(dict)
-    final_dict = get_final_articles_dict()
-
-    for key_category in final_dict:
-        for key_doc in final_dict[key_category]:
-            article_doc = final_dict[key_category][key_doc][0]
-            word_tokenizer[key_category][key_doc] = word_tokenize(article_doc)
-
-    return word_tokenizer
 
 
 def get_sentecizer():
     sentecizer = defaultdict(dict)
-    final_dict = get_final_articles_dict()
 
-    for key_category in final_dict:
-        for key_doc in final_dict[key_category]:
-            article_doc = final_dict[key_category][key_doc][0]
+    for key_category in training_set_dict:
+        for key_doc in training_set_dict[key_category]:
+            article_doc = training_set_dict[key_category][key_doc].orig_doc
 
             sentecizer[key_category][key_doc] = sent_tokenize(article_doc)
 
     return sentecizer
 
 
-def remove_stop_words():
-    # read and put the stop words in a list
+def remove_stop_words(document):
+
     stop_words = []
     with open("stop_words") as fp:
         lines = fp.readlines()
         stop_words = [l.strip() for l in lines]
 
-    print(stop_words)
-    print('_____________')
-
-    # removing stop words with nltk
-    removed_stopwords_articles = defaultdict(dict)
-    word_tokenizer = get_word_tokenizer()
-
-    for key_category in word_tokenizer:
-        for key_doc in word_tokenizer[key_category]:
-            # print(word_tokens)
-            # print('___________________')
-            word_tokens = word_tokenizer[key_category][key_doc]
-            removed_stopwords_articles[key_category][key_doc] = [
-                w for w in word_tokens if not w in stop_words]
-
-    print(removed_stopwords_articles['business']['006.txt'])
-    return removed_stopwords_articles
+    return [w for w in document.word_tokenizer if not w in stop_words]
 
 
-# remove_stop_words()
+print(len(training_set_dict['business']['001.txt'].word_tokenizer))
+print(len(remove_stop_words(training_set_dict['business']['001.txt'])))
 
 
 def get_wordnet_pos(word):
@@ -141,24 +120,118 @@ def get_wordnet_pos(word):
     return tag_dict.get(tag, wordnet.NOUN)
 
 
-def get_lemmatizer():
+def get_lemmatizer(document):
     lemmatizer = WordNetLemmatizer()
-    word_tokenizer = get_word_tokenizer()
-    final_dict = get_final_articles_dict()
-    lemmatizer_dict = defaultdict(dict)
-    # sentence = "The striped bats are hanging on their feet for best"
-    # for key_category in final_dict:
-    #     for key_doc in final_dict[key_category]:
-    #         article_doc = final_dict[key_category][key_doc][0]
-    #         lemmatizer_dict[key_category][key_doc] = [lemmatizer.lemmatize(w, get_wordnet_pos(w))
-    #                                                   for w in word_tokenizer[key_category][key_doc]]
-    my_list = word_tokenizer['business']['006.txt']
-    my_list = [w.lower() for w in my_list]
-    print(word_tokenizer['business']['006.txt'])
-    print('_________________________')
-    print([lemmatizer.lemmatize(w, get_wordnet_pos(w))
-           for w in my_list])
-    # print(lemmatizer_dict['business']['006.txt'])
+    document.word_tokenizer = [w.lower() for w in document.word_tokenizer]
+    return [lemmatizer.lemmatize(w, get_wordnet_pos(w))
+            for w in document.word_tokenizer]
 
 
-get_lemmatizer()
+# print(get_lemmatizer(training_dict['business']['001.txt']))
+
+
+ck = ['business', 'entertainment', 'politics', 'sport', 'tech']
+
+total_docs = sum(len(training_set_dict[k]) for k in ck)
+
+class_probability = {k: len(training_set_dict[k]) / total_docs for k in ck}
+
+alpha = 1
+vocabulary_dim = 0
+
+total_words_ck = defaultdict(list)
+word_probability = defaultdict(dict)
+
+
+word_occurence = defaultdict(dict)
+total_words = []
+
+for key_class in training_set_dict:
+    for key_doc in training_set_dict[key_class]:
+        total_words_ck[key_class] += training_set_dict[key_class][key_doc].word_tokenizer
+
+    total_words += list(set(total_words_ck[key_class]))
+
+    word_freq_ck = Counter(list(total_words_ck[key_class]))
+    for unique_w in word_freq_ck.keys():
+        word_occurence[unique_w][key_class] = word_freq_ck[unique_w]
+
+vocabulary_dim = len(total_words)
+
+# solving KeyError for certain words in certain classes
+
+
+for w in total_words:
+    for c in ck:
+        try:
+            x = word_occurence[w][c]
+        except KeyError:
+            word_occurence[w][c] = 0
+
+
+for unique_w in word_occurence:
+    for c in word_occurence[unique_w]:
+        word_probability[unique_w][c] = (
+            word_occurence[unique_w][c] + alpha) / (len(total_words_ck[c]) + vocabulary_dim + alpha)
+
+
+class_prediction = defaultdict(dict)
+
+
+def predict_class(document):
+
+    doc_predict_sum = defaultdict(dict)
+    for c in ck:
+        log_likelihood = 0
+        for w in list(set(document.word_tokenizer)):
+            try:
+                log_likelihood += math.log(word_probability[w][c])
+            except KeyError:
+                print(w + ' - ' + c)
+
+        doc_predict_sum[c] = class_probability[c] + log_likelihood
+
+    return max(doc_predict_sum.items(), key=operator.itemgetter(1))[0]
+
+
+print(predict_class(training_set_dict['business']['001.txt']))
+print(predict_class(training_set_dict['entertainment']['001.txt']))
+
+
+def predict(test_set_dict):
+    """Predict target values for test set.
+    :param test_set: Test set with dimension m x n,
+                     where m is the number of examples,
+                     and n is the number of features.
+    :return: Predicted target values for test set with dimension m,
+             where m is the number of examples.
+    """
+
+    predictions = defaultdict(dict)
+    for key_class in test_set_dict:
+        for key_doc in test_set_dict[key_class]:
+            result = predict_class(test_set_dict[key_class][key_doc])
+            predictions[key_class][key_doc] = result
+
+    return predictions
+
+
+def get_precision(test_set_dict, predictions):
+    """Get precision of predictions on test set.
+    :param test_set: The set of records to test the model with.
+    :param predictions: Predictions for the test set.
+    :return: The precision of the model.
+    """
+    num_correct = 0
+
+    num_predictions = sum(len(test_set_dict[c].values()) for c in ck)
+
+    for key_class in test_set_dict:
+        for key_doc in test_set_dict[key_class]:
+            if key_class == predictions[key_class][key_doc]:
+                num_correct += 1
+
+    return num_correct / num_predictions
+
+
+print(get_precision(test_set_dict, predict(test_set_dict)))
